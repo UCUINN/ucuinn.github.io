ef18528e2db2ed8a96b2840fa23e9e1d34833573
UCU INN <ucucenter@gmail.com>
2025-09-17 16:59:10 +0300

    Updates

--- assets/Booking-B1O6kNZL.js
+++ assets/Booking-B1O6kNZL.js
@@ -1,4 +1,4 @@
-import{c as r,u as x,r as a,j as e,C as u}from"./index-BUaKPUQM.js";/**
+import{c as r,u as x,r as a,j as e,C as u}from"./index-Bq7acMff.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
